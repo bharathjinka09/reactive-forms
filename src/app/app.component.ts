2327888eb4148ff9ba0f6f9425e864fbcf5eea20
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-11 12:41:21 +0530

    Reactive Forms Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { forbiddenNameValidator } from './shared/user-name.validator';
+import { PasswordValidator } from './shared/password.validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms';
+
+  registrationForm: FormGroup;
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group(
+      {
+        userName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            forbiddenNameValidator(/password/),
+          ],
+        ],
+        email: [''],
+        subscribe: false,
+        password: [''],
+        confirmPassword: [''],
+        address: this.fb.group({
+          city: [''],
+          state: [''],
+          postalCode: [''],
+        }),
+      },
+      { validator: PasswordValidator }
+    );
+
+    this.registrationForm
+      .get('subscribe')
+      .valueChanges.subscribe((checkedValue) => {
+        const email = this.registrationForm.get('email');
+        if (checkedValue) {
+          email.setValidators(Validators.required);
+        } else {
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      });
+  }
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   }),
+  // });
+
+  loadApiData() {
+    // setValue method for all fields required
+    // this.registrationForm.setValue({
+    //   userName: 'Bharath',
+    //   password: 'test',
+    //   confirmPassword: 'test',
+    //   address: {
+    //     city: 'Bangalore',
+    //     state: 'KA',
+    //     postalCode: '560034',
+    //   },
+    // });
+    // patchValue method for some fields
+    // this.registrationForm.patchValue({
+    //   userName: 'Bharath',
+    //   password: 'test',
+    //   confirmPassword: 'test',
+    // });
+  }
 }
